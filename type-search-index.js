4a8ad54745f38a50b108575ad6519bab04f96add
javiermoral09 <162630662+javiermoral09@users.noreply.github.com>
2024-04-04 20:16:37 +0200

    Generar javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"videojuegoMaestro","l":"Enemigo"},{"p":"videojuegoMaestro","l":"Inventario"},{"p":"videojuegoMaestro","l":"Jugador"},{"p":"videojuegoMaestro","l":"Main"}];updateSearchResults();
